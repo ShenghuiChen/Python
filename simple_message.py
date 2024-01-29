0381495221b0d53f23ee3099f0254c3d87357998
陈生辉 <chenshenghui@4399inc.com>
2024-01-29 16:28:17 +0800

    test1

---
+++ simple_message.py
@@ -0,0 +1,16 @@
+message = "您好， 吃了么？"
+print(message)
+
+message = "Yo what's up ?"
+print(message)
+
+username ="haha"
+date = "1月23日"
+city = 'fj'
+weather = '大风'
+message = f'{username}您好， 今天是{date}, 您所在的{city}天气为{weather}'
+print(message)
+
+name = "Eric".upper()
+print(f"Hello {name}, would  you like to cearn some Python")
+
